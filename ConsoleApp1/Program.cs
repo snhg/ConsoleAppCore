fdf7c8825ffe758da8535799d79a82750a6846a2
Surya <suryahg@gmail.com>
2019-11-27 21:11:47 +0530

    b1:updates

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("main:Hello World!");
+            Console.WriteLine("b1:Hello World!");
 
             Console.WriteLine("b2:Hello World!");
         }
